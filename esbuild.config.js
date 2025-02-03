1e7414098e539c4a798b436407dfa4fe6d91e9f6
Andrew Fong <afong@slack-corp.com>
2025-02-02 20:07:05 -0800

    Fix tests

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,20 +1,32 @@
 const esbuild = require('esbuild');
+const glob = require('glob');
 
 const baseConfig = {
-	entryPoints: ['./src/extension.ts'],
 	bundle: true,
-	outfile: 'out/extension.js',
 	external: ['vscode'],
 	format: 'cjs',
 	platform: 'node',
 	sourcemap: true,
+};
+
+const mainConfig = {
+	...baseConfig,
+	entryPoints: ['./src/extension.ts'],
+	outfile: 'out/extension.js',
 	minify: process.env.NODE_ENV === 'production',
+};
+
+const testConfig = {
+	...baseConfig,
+	entryPoints: glob.sync('./src/**/*.test.ts'),
+	outdir: 'out',
+	minify: false,
 };
 
 // Build for production
 if (process.argv.includes('--production')) {
 	esbuild.buildSync({
-		...baseConfig,
+		...mainConfig,
 		minify: true,
 	});
 }
@@ -23,14 +35,19 @@ if (process.argv.includes('--production')) {
 else if (process.argv.includes('--watch')) {
 	esbuild
 		.context({
-			...baseConfig,
+			...mainConfig,
 		})
 		.then((ctx) => {
 			ctx.watch();
 		});
 }
 
+// Build tests
+else if (process.argv.includes('--tests')) {
+	esbuild.buildSync(testConfig);
+}
+
 // Regular build
 else {
-	esbuild.buildSync(baseConfig);
+	esbuild.buildSync(mainConfig);
 }
