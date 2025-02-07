fa9826384e1dbe15d30ca19e9b593d61e6de37d9
Andrew Fong <afong@slack-corp.com>
2025-02-07 09:22:20 -0800

    Fix ESBuild + debug

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,6 +2,27 @@ const esbuild = require('esbuild');
 const glob = require('glob');
 const fs = require('fs');
 const path = require('path');
+const plugin = require('eslint-plugin-mocha');
+
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const esbuildProblemMatcherPlugin = {
+	name: 'esbuild-problem-matcher',
+
+	setup(build) {
+		build.onStart(() => {
+			console.log('[watch] build started');
+		});
+		build.onEnd((result) => {
+			result.errors.forEach(({ text, location }) => {
+				console.error(`✘ [ERROR] ${text}`);
+				console.error(`    ${location.file}:${location.line}:${location.column}:`);
+			});
+			console.log('[watch] build finished');
+		});
+	},
+};
 
 const OUT_DIR = 'out';
 
@@ -11,6 +32,7 @@ const baseConfig = {
 	format: 'cjs',
 	platform: 'node',
 	sourcemap: true,
+	plugins: [esbuildProblemMatcherPlugin],
 };
 
 const mainConfig = {
@@ -30,31 +52,33 @@ const testConfig = {
 // Delete the out directory before build
 fs.rmSync(OUT_DIR, { recursive: true, force: true });
 
-// Build for production
-if (process.argv.includes('--production')) {
-	esbuild.buildSync({
-		...mainConfig,
-		minify: true,
-	});
-}
+async function main() {
+	let config = mainConfig;
 
-// Watch mode
-else if (process.argv.includes('--watch')) {
-	esbuild
-		.context({
+	// Build for production
+	if (process.argv.includes('--production')) {
+		config ={
 			...mainConfig,
-		})
-		.then((ctx) => {
-			ctx.watch();
-		});
+			minify: true,
+			sourcemap: false,
+		};
+	}
+
+	// Build for test
+	else if (process.argv.includes('--test')) {
+		config = testConfig
+	}
+
+	const ctx = await esbuild.context(config);
+	if (process.argv.includes('--watch')) {
+		await ctx.watch();
+	} else {
+		await ctx.rebuild();
+		await ctx.dispose();
+	}
 }
 
-// Build tests
-else if (process.argv.includes('--test')) {
-	esbuild.buildSync(testConfig);
-}
-
-// Regular build
-else {
-	esbuild.buildSync(mainConfig);
-}
+main().catch((e) => {
+	console.error(e);
+	process.exit(1);
+});
